7caf6ca8198f6c90457de4558ceaa80f640347eb
BONFY <foreverbonfy@163.com>
2016-10-13 16:57:41 +0800

    add 016-017

---
+++ 016-3sum-closest.py
@@ -0,0 +1,42 @@
+# -*-coding:utf-8-*-
+
+# Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+#     For example, given array S = {-1 2 1 -4}, and target = 1.
+
+#     The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+# python version: Python 3
+
+
+__Auther__ = 'BONFY'
+
+
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        result = sum(nums[:3])
+        for i in range(len(nums) - 2):
+            j, k = i+1, len(nums) - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == target:
+                    return s
+
+                if abs(s - target) < abs(result - target):
+                    result = s
+
+                if s < target:
+                    j += 1
+                elif s > target:
+                    k -= 1
+        return result
+
+
+if __name__ == '__main__':
+    print(Solution().threeSumClosest([-1, 2, 1, -4], 1))
